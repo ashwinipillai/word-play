09f1960b5f63449d1b3975f5a88bea1b83c0ed8b
Ashwini Pillai <ashwini@Ashwinis-MacBook-Air.local>
2022-02-10 21:33:15 +0530

    Added support for all combinations from 3-letter words to n-letter words

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ import json
 import enchant
 from itertools import permutations
 
+
+def convertTuple(tup):
+    # initialize an empty string
+    str = ''
+    for item in tup:
+        str = str + item
+    return str
+
 def word_exists(w):
     # converts to lower case
     obj = enchant.Dict("en_US")
@@ -15,9 +23,14 @@ def word_exists(w):
 # print("How many letters do you want to play the Jumble Game for?")
 # no_of_letters = int(input())
 # letters = ''.join((random.choice(string.ascii_lowercase) for x in range(no_of_letters)))
-print("Enter letters: ")
+print("Enter letters separated by SPACE ; Press ENTER when you are done. ")
 letters = input().split(" ")
-all_permutations =[''.join(p) for p in permutations(letters)]
+all_permutations=[]
+
+for a in range(3,len(letters)+1):
+    words = list(permutations(letters,a))
+    for w in words:
+        all_permutations.append(convertTuple(w))
 for each_combination in all_permutations:
     output = word_exists(each_combination)
     if output:
